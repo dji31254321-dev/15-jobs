2309d13a08aea8afc0b21420faaf8a0ce12e19a0
dji31254321 <dji31254321@gmail.com>
2026-05-12 17:50:48 +0900

    add saramin scrapper

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, send_file
-from scrapper import search_incruit
+from scrapper import search_incruit, search_saramin
 from file import save_to_csv
+
+
+
 
 app = Flask(__name__)
 
@@ -11,15 +14,17 @@ def hello_world():
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
-    jobs = search_incruit(keyword,1)
-    return render_template("search.html", jobs=enumerate(jobs), keyword=keyword)
+    saramin_jobs = search_saramin(keyword,1)
+    incruit_jobs = search_incruit(keyword,1)
+    all_jobs = saramin_jobs + incruit_jobs
+    return render_template("search.html", jobs=enumerate(all_jobs), keyword=keyword)
 
 
 @app.route("/file")
 def file():
     keyword = request.args.get("keyword")
-    jobs = search_incruit(keyword,1)
-    save_to_csv(jobs)
+    all_jobs = search_incruit(keyword,1) + search_saramin(keyword,1)
+    save_to_csv(all_jobs)
     return send_file("downloads.csv", as_attachment=True)
 
 
--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,37 @@ from bs4 import BeautifulSoup
 
 
 
+def search_saramin(keyword, pages):
+    jobs = []
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36"}
+    for i in range(pages):
+        page_num = i + 1
+        url = f"https://www.saramin.co.kr/zf_user/search/recruit?search_area=main&search_done=y&search_optional_item=n&searchType=search&searchword={keyword}&recruitPage={page_num}&recruitSort=relation&recruitPageCount=40&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting="
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
+        lis = soup.find_all("div", class_="item_recruit")
+
+        for li in lis:
+            company = li.find("div", class_ = "area_corp").find("a").text.strip()
+            title_tag = li.find("div", class_ = "area_job").find("h2", class_ = "job_tit").find("a")
+            title = title_tag.text.strip()
+            location = li.find("div", class_ = "job_condition").find_all("span")[0].text.strip()
+            
+            link = "https://www.saramin.co.kr" + title_tag.get("href")
+            
+            job_data = {
+                "company": company,
+                "title": title,
+                "location": location,
+                "link": link
+            }
+
+            jobs.append(job_data)
+            
+    return jobs
+
+
+
 def search_incruit(keyword, pages):     
 
     jobs = []
