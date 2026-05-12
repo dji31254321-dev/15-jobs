7919651c5e613ac675c6e1e343b31dcdfa612e5b
dji31254321 <dji31254321@gmail.com>
2026-05-12 15:44:07 +0900

    0512 15:43

--- scrapper.py
+++ scrapper.py
@@ -1,23 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
+
+
+
+def search_incruit(keyword, pages):     
+
+    jobs = []
+    for i in range(pages):
+        page = i * 30
+        
+        url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno=1{page}"
+        print(url)
+
+
+        response = requests.get(url)
+
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+
+        lis = soup.find_all("li", class_ = "c_col")
+
+
+
+
+
+
+        for li in lis:
+            
+            company = li.find("a", class_ = "cpname").text.strip()
+            title = li.find("div", class_ = "cell_mid").find("div", class_ = "cl_top").find("a").text.strip()
+            location = li.find("div", class_ = "cl_md").find_all("span")[0].text.strip()
+            link = li.find("div", class_ = "cell_mid").find("div", class_ = "cl_top").find("a").get("href")
+            
+            job_data = {
+                "company": company,
+                "title": title,
+                "location": location,
+                "link": link
+            }
+
+            jobs.append(job_data)
+    return jobs
+
 keyword = "파이썬"
-url = f"https://search.incruit.com/list/search.asp?col=job&kw={keyword}&startno=0"
+url = f"https://www.jobkorea.co.kr/Search?stext={keyword}&tabType=recruit&Page_No=1"
+
 response = requests.get(url)
-#print(response.text)
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-
-lis = soup.find_all("li", class_ = "c_col")
-# print(len(lis))
-# print(lis[0])
-for li in lis[0:1]:
-    # title = li.find("a", class_ = "c_title")
-    # company = li.find("a", class_ = "cpname").text.strip()
-    # print(company)
-    # title = li.find("div", class_ = "cl_mid").find("div", class_ = "cl_top").find("a").text.strip()
-    # print(title)
-    # location = li.find("div", class_ = "cl_md").find_all("span")[0].text.strip()
-    # print(location)
-    link = li.find("div", class_ = "cell_mid").find("div", class_ = "cl_top").find("a").get("href")
-    print(link)
